b46de9b8dcfe55204736d43414d747f931a5e260
Igor_U <fixtor75@gmail.com>
2022-08-05 21:41:43 +0300

    Упражнение 2 сделано

--- Exe2/Program.cs
+++ Exe2/Program.cs
@@ -1,9 +1,8 @@
-﻿int a; // первое число
-int b; // второе число
+﻿Console.Clear();
 Console.Write("Введите чилcо a ");
-a = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число b ");
-b = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
 if (a < b)
     Console.WriteLine("Максимальное число " + b + "\n");
     else 
