30b45d4176328486267e7968d4577358a805bdf2
Igor_U <fixtor75@gmail.com>
2022-08-05 21:40:21 +0300

    сделано задание 4

---
+++ Exe4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите чилcо a ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите чилcо b ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите чилcо c ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a < b && a < c)
+{
+    if (b < c)
+    Console.WriteLine(c);
+    else Console.WriteLine(b);
+}
+else Console.WriteLine(a);
