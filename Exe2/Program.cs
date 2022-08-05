b8bfa4593b2fe0af97ab950a3049a9568e4de29f
Igor_U <fixtor75@gmail.com>
2022-08-05 20:57:26 +0300

    Сдклано упражнение 2 и доюавлен git ignor

---
+++ Exe2/Program.cs
@@ -0,0 +1,11 @@
+﻿int a; // первое число
+int b; // второе число
+Console.Write("Введите чилcо a ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b ");
+b = Convert.ToInt32(Console.ReadLine());
+if (a < b)
+    Console.WriteLine("Максимальное число " + b + "\n");
+    else 
+        Console.WriteLine("Maксимальное число " + a + "\n");
+    
