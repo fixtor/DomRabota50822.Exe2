d09d71230369f986588e019b35293fd3a4a8bcb1
Igor_U <fixtor75@gmail.com>
2022-08-06 00:23:21 +0300

    Update Program.cs
    Добавлено нажатие

--- Exe2/Program.cs
+++ Exe2/Program.cs
@@ -4,6 +4,7 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число b ");
 int b = Convert.ToInt32(Console.ReadLine());
 if (a < b)
-    Console.WriteLine("Максимальное число " + b + "\n");
+    Console.WriteLine("Максимальное число " + b);
     else 
-        Console.WriteLine("Maксимальное число " + a + "\n");
+        Console.WriteLine("Maксимальное число " + a);
+Console.ReadKey();
