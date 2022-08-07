d087dd18798d92e3e0b9bef06929d11c27b42b3e
Igor_U <fixtor75@gmail.com>
2022-08-07 20:59:41 +0300

    Update Program.cs
    Теперь проверка от 1

--- Exe8/Program.cs
+++ Exe8/Program.cs
@@ -2,7 +2,7 @@
 int a = 0;
 Console.WriteLine("введите любое число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-for (a = 0; a <= n; a++)
+for (a = 1; a <= n; a++)
 {
     if (a % 2 == 0)
         Console.Write(a + " ");
