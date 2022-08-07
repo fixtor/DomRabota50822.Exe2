79ce49d8d6af88c73c90c9568c21399465249251
Igor_U <fixtor75@gmail.com>
2022-08-07 17:09:40 +0300

    Задание готово

---
+++ extraEx2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Random rnd = new Random();
+int n = rnd.Next(20, 90);
+Console.WriteLine("n: " + n + "\n");
+Console.WriteLine("Введите случайное число: ");
+int d = Convert.ToInt32(Console.ReadLine());
+string mes1 = (n % d == 0) ? "Делится" : "не делится";
+Console.WriteLine("Число {0} {1}", n, mes1);
+Console.ReadKey();
