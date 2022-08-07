0506477b6da9d4e60d07a0d82e1c9efcdb924eee
Igor_U <fixtor75@gmail.com>
2022-08-07 17:11:21 +0300

    Updated

--- extraEx2/Program.cs
+++ extraEx2/Program.cs
@@ -2,7 +2,7 @@
 Random rnd = new Random();
 int n = rnd.Next(20, 90);
 Console.WriteLine("n: " + n + "\n");
-Console.WriteLine("Введите случайное число: ");
+Console.WriteLine("Проверим деление. Введите случайное число: ");
 int d = Convert.ToInt32(Console.ReadLine());
 string mes1 = (n % d == 0) ? "Делится" : "не делится";
 Console.WriteLine("Число {0} {1}", n, mes1);
