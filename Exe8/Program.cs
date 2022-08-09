514786c220632329fd26b27c866319a979c54650
Igor_U <fixtor75@gmail.com>
2022-08-09 23:57:24 +0300

    upd...

--- Exe8/Program.cs
+++ Exe8/Program.cs
@@ -4,7 +4,8 @@ Console.WriteLine("введите любое число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 for (a = 1; a <= n; a++)
 {
+//Console.WriteLine($"Тест {a}      {n}");
     if ((a % 2) == 0)
-        Console.Write(a + " ");
+        Console.Write($" {a} ");
 }
 Console.ReadKey();
